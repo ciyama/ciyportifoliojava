035d89b17fff68e46c6418a45895cd5511abcc6b
Celio Iyama <celio.iyama@gmail.com>
2021-08-03 21:43:55 -0300

    Primeiro commit: Projeto criado

---
+++ src/main/java/com/ciyama/ciyportifliojava/CiyportifoliojavaApplication.java
@@ -0,0 +1,13 @@
+package com.ciyama.ciyportifliojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiyportifoliojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiyportifoliojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciyama/ciyportifliojava/CiyportifoliojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciyama.ciyportifliojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiyportifoliojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
