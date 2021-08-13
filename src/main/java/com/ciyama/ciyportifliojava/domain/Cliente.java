413f9fa21438fbec0ab6b0b5cbe81a9935512fcc
Celio Iyama <celio.iyama@gmail.com>
2021-08-12 22:03:29 -0300

    Pedido, EstadoPagamento e Pagamento (@OneToOne)

--- src/main/java/com/ciyama/ciyportifliojava/domain/Cliente.java
+++ src/main/java/com/ciyama/ciyportifliojava/domain/Cliente.java
@@ -38,6 +38,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -106,6 +108,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -122,7 +132,7 @@ public class Cliente implements Serializable {
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 	
 	
 	
---
+++ src/main/java/com/ciyama/ciyportifliojava/domain/PagamentoComCartao.java
@@ -0,0 +1,29 @@
+package com.ciyama.ciyportifliojava.domain;
+
+import com.ciyama.ciyportifliojava.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento {
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+		}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+
+	
+}
